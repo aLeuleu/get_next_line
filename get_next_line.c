5a477e95ce41ff7d7bd1742d86beb5a61ada00f5
Aurelien <aurelien.levra@gmail.com>
2022-11-14 02:19:35 +0100

    asdf

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,15 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 10:02:39 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/14 01:09:03 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/11/14 02:13:26 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdint.h>
+#include <stdio.h>
 
 void	*ft_memset(void *pointer, int value, size_t count)
 {
@@ -41,7 +42,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 		return (NULL);
 	return (ft_memset(res, 0, nmemb * size));
 }
-
 
 size_t	ft_strlen(const char *s)
 {
@@ -88,17 +88,18 @@ static int	contains_endl(char *str)
 	size_t	i;
 
 	i = -1;
-	while(str && str[++i])
+	while (str && str[++i])
 		if (str[i] == '\n' || str[i] < 0)
-			return (1);
-	return (0);
+			return ( str[i] == '\n' ?  printf("Le buffer contient endl !\n") : printf("Le buffer contient EOF !\n") ,1);
+	return (printf("Le buffer ne contient pas endl ou eof \n"),0);
+
 }
 
 static char	*read_buffer(int fd, size_t buffer_size)
 {
 	char	*buff;
 
-	buff = (char *)malloc(sizeof(char) * (buffer_size + 1) );
+	buff = (char *)malloc(sizeof(char) * (buffer_size + 1));
 	read(fd, buff, buffer_size);
 	buff[buffer_size + 1] = '\0';
 	return (buff);
@@ -108,7 +109,7 @@ char	*realloc_by_buffer(char *oldstr, size_t newstr_size)
 {
 	char	*newstr;
 
-	newstr = (char *)ft_calloc(newstr_size ,sizeof(char));
+	newstr = (char *)ft_calloc(newstr_size, sizeof(char));
 	if (!newstr)
 	{
 		if (oldstr)
@@ -127,12 +128,12 @@ char	*realloc_by_buffer(char *oldstr, size_t newstr_size)
 #include <stddef.h>
 #include <stdio.h>
 
-int		strlen_untill(char *str, char delimiter)
+int	strlen_untill(char *str, char delimiter)
 {
-	size_t len;
+	size_t	len;
 
 	len = 0;
-	while (str && str[len] > 0 && str[len] != delimiter )
+	while (str && str[len] > 0 && str[len] != delimiter)
 		len++;
 	return (len);
 }
@@ -143,34 +144,48 @@ char	*get_next_line(int fd)
 	char	*buffer;
 	char	*next_line;
 	size_t	i;
-
-	i = 1;
+ 
+	i = 0;
 	buffer_size = 10;
 	buffer = NULL;
-    next_line = NULL;
- 	while (!contains_endl(buffer))
+	next_line = NULL;
+	while (!contains_endl(buffer) && i < 10)
 	{
+        printf("\n\n----------------\nboucle %ld\n", i );
+        printf("allocation de next_line : %ld\n", buffer_size * i);
 		next_line = realloc_by_buffer(next_line, buffer_size * i);
-		ft_strlcat(next_line,buffer,buffer_size * i);		
+		printf("next_line : %s\n", next_line);
+        write(1,"ancien buffer :", 15 );
+        write(1,buffer, buffer_size );
+        write(1,"\n",1);
+        printf("strlcat ..\n");
+   		ft_strlcat(next_line, buffer, buffer_size * i);
+		printf("next_line : %s\n", next_line);
 		buffer = read_buffer(fd, buffer_size);
+        write(1,"nouveau buffer :", 16 );
+        write(1,buffer, buffer_size );
+        write(1,"\n",1);
+
 		i++;
 	}
- 	next_line = realloc_by_buffer(next_line, (buffer_size * (i - 2)) + strlen_untill(buffer, '\n') + 1);
-	ft_strlcat(next_line, buffer, (buffer_size * (i - 2)) + strlen_untill(buffer, '\n') + 1);
-    free(buffer);
+	next_line = realloc_by_buffer(next_line, (buffer_size * (i - 1))
+			+ strlen_untill(buffer, '\n') + 1);
+	ft_strlcat(next_line, buffer, (buffer_size * (i - 1))
+			+ strlen_untill(buffer, '\n') + 1);
+	free(buffer);
 	return (next_line);
 }
 
 int	main(int argc, char const *argv[])
 {
-	int fd;
+	int	fd;
+
 	fd = open("./mon_fichier.txt", O_RDONLY);
 	printf("1 : %s\n", get_next_line(fd));
 	printf("2 : %s\n", get_next_line(fd));
 	printf("3 : %s\n", get_next_line(fd));
-    printf("4 : %s\n", get_next_line(fd));
-    printf("5 : %s\n", get_next_line(fd));
-    printf("6 : %s\n", get_next_line(fd));
-    
+	printf("4 : %s\n", get_next_line(fd));
+	printf("5 : %s\n", get_next_line(fd));
+	printf("6 : %s\n", get_next_line(fd));
 }
 // todo gerer les cas ou le file est deja entierement read
