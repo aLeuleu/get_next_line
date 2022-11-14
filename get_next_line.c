71abf69543ef2cd7172de968080b50abc370c3a0
Aurelien <aurelien.levra@gmail.com>
2022-11-14 16:30:11 +0100

    asdf

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 10:02:39 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/14 02:13:26 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/11/14 12:23:45 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memset(void *pointer, int value, size_t count)
 	unsigned char	*ptr;
 	size_t			i;
 
+	if (count == 0)		//debug
+		return (NULL); //debug
 	ptr = (unsigned char *)pointer;
 	i = 0;
 	while (i < count)
@@ -35,6 +37,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*res;
 
+	if (!nmemb || !size) //debug
+		return (NULL); //debug
 	if (nmemb > SIZE_MAX / size)
 		return (NULL);
 	res = malloc(nmemb * size);
@@ -109,7 +113,11 @@ char	*realloc_by_buffer(char *oldstr, size_t newstr_size)
 {
 	char	*newstr;
 
-	newstr = (char *)ft_calloc(newstr_size, sizeof(char));
+	if (!newstr_size)
+		newstr =  (char *)ft_calloc(1, sizeof(char));
+	else
+		newstr = (char *)ft_calloc(newstr_size, sizeof(char));
+	printf("newstr : \"%s\"\n", newstr);
 	if (!newstr)
 	{
 		if (oldstr)
@@ -145,33 +153,40 @@ char	*get_next_line(int fd)
 	char	*next_line;
 	size_t	i;
  
-	i = 0;
+	i = 0; 
 	buffer_size = 10;
 	buffer = NULL;
 	next_line = NULL;
 	while (!contains_endl(buffer) && i < 10)
 	{
-        printf("\n\n----------------\nboucle %ld\n", i );
-        printf("allocation de next_line : %ld\n", buffer_size * i);
+			printf("\n\n----------------\nboucle %ld\n", i );
+			printf("allocation de next_line : %ld\n", buffer_size * i);
 		next_line = realloc_by_buffer(next_line, buffer_size * i);
-		printf("next_line : %s\n", next_line);
-        write(1,"ancien buffer :", 15 );
-        write(1,buffer, buffer_size );
-        write(1,"\n",1);
-        printf("strlcat ..\n");
-   		ft_strlcat(next_line, buffer, buffer_size * i);
-		printf("next_line : %s\n", next_line);
+		{
+			printf("next_line : \"%s\"\n", next_line);
+			write(1,"ancien buffer :\"", 16 );
+			write(1,buffer, buffer_size );
+			write(1,"\"\n",2);
+			printf("strlcat ..\n");
+		}
+			
+   		ft_strlcat(next_line, buffer, (buffer_size * i) + 1);
+			printf("next_line : \"%s\"\n", next_line);
+			
 		buffer = read_buffer(fd, buffer_size);
-        write(1,"nouveau buffer :", 16 );
-        write(1,buffer, buffer_size );
-        write(1,"\n",1);
-
+			write(1,"nouveau buffer :\"", 17 );
+			write(1,buffer, buffer_size );
+			write(1,"\"\n\n",3);
+	
 		i++;
 	}
-	next_line = realloc_by_buffer(next_line, (buffer_size * (i - 1))
-			+ strlen_untill(buffer, '\n') + 1);
-	ft_strlcat(next_line, buffer, (buffer_size * (i - 1))
-			+ strlen_untill(buffer, '\n') + 1);
+		printf("\n\n----------------\nboucle %ld (fin)\n", i );
+		printf("allocation de next_line : %ld\n", (buffer_size * (i-1)) + strlen_untill(buffer, '\n') +1);
+	next_line = realloc_by_buffer(next_line, (buffer_size * (i-1))
+			+ strlen_untill(buffer, '\n') +1);
+	ft_strlcat(next_line, buffer, (buffer_size * (i-1))
+			+ strlen_untill(buffer, '\n') +1);
+		printf("next_line : \"%s\"\n", next_line);
 	free(buffer);
 	return (next_line);
 }
@@ -182,10 +197,18 @@ int	main(int argc, char const *argv[])
 
 	fd = open("./mon_fichier.txt", O_RDONLY);
 	printf("1 : %s\n", get_next_line(fd));
+	printf("************\n\n\n");
 	printf("2 : %s\n", get_next_line(fd));
+	printf("************\n\n\n");
 	printf("3 : %s\n", get_next_line(fd));
+	printf("************\n\n\n");
 	printf("4 : %s\n", get_next_line(fd));
+	printf("************\n\n\n");
 	printf("5 : %s\n", get_next_line(fd));
+	printf("************\n\n\n");
 	printf("6 : %s\n", get_next_line(fd));
+	printf("************\n\n\n");
 }
+
 // todo gerer les cas ou le file est deja entierement read
+// mon calloc ne set pas bien la memoire a zero !
